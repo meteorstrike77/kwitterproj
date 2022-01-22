1648a2814a4060e3aa131ab43a49d5e7787ff37e
meteorstrike77 <89230784+meteorstrike77@users.noreply.github.com>
2022-01-22 10:28:51 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBIEn76GuDoLiGwIM5lVxm37XLeSYyAQp4",
+    authDomain: "let-s-chat-project-5dc7d.firebaseapp.com",
+    projectId: "let-s-chat-project-5dc7d",
+    storageBucket: "let-s-chat-project-5dc7d.appspot.com",
+    messagingSenderId: "317968945484",
+    appId: "1:317968945484:web:633bd0afa0e5e9a9bcbfe9"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
