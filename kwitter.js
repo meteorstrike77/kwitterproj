1c8c30d4175dd1443cadcae5e23082cb204a4f10
meteorstrike77 <89230784+meteorstrike77@users.noreply.github.com>
2022-01-26 19:11:11 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,5 @@
-function addUser () {
+function addUser() {
     username = document.getElementById("user_name").value;
-    localStorage.setItem("username", username);
+    localStorage.setItem("user_name", username);
     window.location = "kwitter_room.html"
 }
